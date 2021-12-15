68c9321bfc545972bf61320f42688a334a13777d
saicherry <saicherry196@gmail.com>
2021-12-15 21:46:51 +0530

    Check Point7

---
+++ config/models/artist.model.js
@@ -0,0 +1,16 @@
+module.exports = mongoose => {
+    const Artist = mongoose.model(
+        "artist",
+        mongoose.Schema(
+          {
+            artistid:Number,
+            first_name:String,
+            last_name:String,
+            wiki_url:String,
+            profile_url:String,
+            movies: [String]
+          }
+        )
+      );
+      return Artist;
+    };
